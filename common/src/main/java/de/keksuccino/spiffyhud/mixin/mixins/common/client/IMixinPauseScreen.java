6557535d9a4e735d405ba95dd93b051e53b7d5d5
Commit Bot - Keksuccino <commit_bot@keksuccino.com>
2025-04-23 00:17:29 +0200

    v3.0.0 - 8

---
+++ common/src/main/java/de/keksuccino/spiffyhud/mixin/mixins/common/client/IMixinPauseScreen.java
@@ -0,0 +1,12 @@
+package de.keksuccino.spiffyhud.mixin.mixins.common.client;
+
+import net.minecraft.client.gui.screens.PauseScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(PauseScreen.class)
+public interface IMixinPauseScreen {
+
+    @Accessor("showPauseMenu") boolean get_showPauseMenu_Spiffy();
+
+}
